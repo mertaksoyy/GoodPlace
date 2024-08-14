4f0c9dc9cca2db4883e4e1935d255a7a4d70b0e8
celikerde <celikerde@itu.edu.tr>
2024-08-14 17:28:56 +0300

    drawer is added to main screen.

--- goodplace/lib/views/main_screen_view.dart
+++ goodplace/lib/views/main_screen_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:goodplace/constants/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreenView extends StatefulWidget {
   const MainScreenView({super.key});
@@ -13,15 +14,46 @@ class _MainScreenViewState extends State<MainScreenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff8E97FD),
+        title: Text(
+          "Habits",
+          style: GoogleFonts.rubik(
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic,
+              fontSize: 25),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacementNamed(welcomePageRoute);
+              },
+            ),
+          ],
+        ),
+      ),
       body: const Center(
         child: Text('User is logged in with email and password.'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseAuth.instance.signOut();
-          Navigator.of(context).pushReplacementNamed(welcomePageRoute);
-        },
-        child: Text('Log Out'),
       ),
     );
   }
